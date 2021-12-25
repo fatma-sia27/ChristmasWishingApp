b3e2717dda0de55d8f5677c3a254798ac65f7086
fatma-sia27 <fatma.sia2020@gmail.com>
2021-12-25 12:59:22 +0530

    ChristmasWishingApp

---
+++ app/src/main/java/com/example/christmaswishingapp/XmasGreeting.kt
@@ -0,0 +1,14 @@
+package com.example.christmaswishingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class XmasGreeting : AppCompatActivity() {
+    companion object {
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_xmas_greeting)
+    }
+}
